4bef845a188d3004829c4e293e1ddd2cae225a06
haikal4002 <230411100095@student.trunojoyo.ac.id>
2025-07-12 07:21:41 +0700

    update

--- src/handler.js
+++ src/handler.js
@@ -103,7 +103,7 @@ const deleteNoteByIdHandler = (request, h) => {
   if (index !== -1) {
     notes.splice(index, 1);
     const response = h.response({
-      status: 'succcess',
+      status: 'success',
       message: 'Catatan berhasil dihapus',
     });
     response.code(200);
